40c0c68b96f6c8382a8102e878ad73812a0acfcb
Andrew Paterson <andrewpaterson@andrews-air.westell.com>
2013-12-29 13:32:39 -0500

    Initial Commit

---
+++ My DVD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  My DVD
+//
+//  Created by Andrew Paterson on 12/29/13.
+//  Copyright (c) 2013 Andrew Paterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ My DVD/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  My DVD
+//
+//  Created by Andrew Paterson on 12/29/13.
+//  Copyright (c) 2013 Andrew Paterson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
